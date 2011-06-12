fb153f15c27a11d46e27e9c14399e59293aa0a7d
Jilles Oldenbeuving <ojilles@gmail.com>
2011-06-13 01:40:35 +0200

    Added a few small tweaks

--- .justniffer.sh
+++ .justniffer.sh
@@ -2,4 +2,4 @@
 #
 #  http://justniffer.sourceforge.net/man.htm
 #
-sudo justniffer -i en1 -u -l "%request.timestamp(%T %D) - %dest.ip:%dest.port [http://%request.header.host/%request.url] -  %response.code %response.size" 
+sudo justniffer -i en1 -u -l "%request.timestamp(%T %D) - %dest.ip:%dest.port%tab[http://%request.header.host%request.url] -  %response.code %response.size %response.time %connection.time" 
