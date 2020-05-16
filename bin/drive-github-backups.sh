4b1b31a91defb12ed7af679b5e4ad8009d5c7e23
Jilles Oldenbeuving <ojilles@gmail.com>
2020-05-16 23:32:07 +0200

    Update drive-github-backups.sh

--- bin/drive-github-backups.sh
+++ bin/drive-github-backups.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
-REPOS="ojilles/data_hacks ojilles/dotfiles ojilles/hadoop-test ojilles/iPhoneTracker ojilles/jilles.net ojilles/Nordea-importer-for-Yunoo";
+REPOS="ojilles/data_hacks ojilles/dotfiles ojilles/hadoop-test ojilles/heatmaps ojilles/iPhoneTracker ojilles/jilles.net ojilles/Nordea-importer-for-Yunoo";
 
 for repo in $REPOS;
 do
